8154d0101281acb647dadc6e0f3e3f287ae1795c
iknittheweb <iknittheweb@gmail.com>
2025-08-24 13:40:24 -0500

    update the contact form

--- js/forms.js
+++ js/forms.js
@@ -1,101 +1,52 @@
-var form = document.getElementsByTagName("form")[0];
+// Contact form handler for contact.html
+// Collects: name, phone, email, comments, and send_to
+// Submits via AJAX to the form's action URL
+
+const form = document.querySelector("form");
+
 form.addEventListener("submit", function(e) {
   e.preventDefault();
-  sendData();
+  sendContactData();
 });
 
-// https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
-function sendData() {
-  var XHR = new XMLHttpRequest();
-  var urlEncodedData = "";
-  var urlEncodedDataPairs = [];
+function sendContactData() {
+  const XHR = new XMLHttpRequest();
+  const urlEncodedDataPairs = [];
 
   urlEncodedDataPairs.push(
-    encodeURIComponent("name") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='name']").value)
+    encodeURIComponent("name") + "=" + encodeURIComponent(form.querySelector("[name='name']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("send_to") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='send_to']").value)
+    encodeURIComponent("send_to") + "=" + encodeURIComponent(form.querySelector("[name='send_to']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("email") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='email']").value)
+    encodeURIComponent("email") + "=" + encodeURIComponent(form.querySelector("[name='email']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("phone") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='phone']").value)
+    encodeURIComponent("phone") + "=" + encodeURIComponent(form.querySelector("[name='phone']").value)
   );
   urlEncodedDataPairs.push(
-    encodeURIComponent("date") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='date']").value)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("qty") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='qty']").value)
+    encodeURIComponent("comments") + "=" + encodeURIComponent(form.querySelector("[name='comments']").value)
   );
 
-  // radio buttons
-  let radio = document.getElementsByName("ufotype");
-  for (var i = 0, length = radio.length; i < length; i++) {
-    if (radio[i].checked) {
-      urlEncodedDataPairs.push(
-        encodeURIComponent("ufotype") + "=" + encodeURIComponent(radio[i].value)
-      );
-    }
-  }
+  const urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
 
-  // dropdown menu
-  var dropdown = form.querySelector("[name='abtype']");
-  urlEncodedDataPairs.push(
-    encodeURIComponent("abtype") +
-      "=" +
-      encodeURIComponent(dropdown.options[dropdown.selectedIndex].text)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("comments") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='comments']").value)
-  );
-  urlEncodedDataPairs.push(
-    encodeURIComponent("subscribe") +
-      "=" +
-      encodeURIComponent(form.querySelector("[name='subscribe']").checked)
-  );
-
-  // Combine the pairs into a single string and replace all %-encoded spaces to
-  // the '+' character; matches the behaviour of browser form submissions.
-  urlEncodedData = urlEncodedDataPairs.join("&").replace(/%20/g, "+");
-
-  // Define what happens on successful data submission
-  XHR.addEventListener("load", function(event) {
+  XHR.addEventListener("load", function() {
     if (XHR.readyState === XHR.DONE) {
       if (XHR.status === 200) {
-        alert("Your order has been received! Check your email.");
+        alert("Thank you! Your message has been sent.");
+        form.reset();
       } else {
-        alert("Oh oh! We have a problem! " + XHR.responseText + ".");
+        alert("Sorry, there was a problem sending your message. " + XHR.responseText);
       }
     }
   });
 
-  // Define what happens in case of error
-  XHR.addEventListener("error", function(event) {
-    // This is normally a timeout or connection error.
-    alert("Oops! Something went wrong.");
+  XHR.addEventListener("error", function() {
+    alert("Oops! Something went wrong. Please try again later.");
   });
 
-  // Set up our request
   XHR.open(form.getAttribute("method"), form.getAttribute("action"));
-
-  // Add the required HTTP header for form data POST requests
   XHR.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  // Finally, send our data.
   XHR.send(urlEncodedData);
 }
